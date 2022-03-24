c1e4368217e87eaaaa6b962ad91fb03e713cbf35
jawad <wadja.b@hotmail.fr>
2022-03-24 15:57:35 +0100

    creation du projet et installation de react router et redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { Route } from "react-router";
+import './style.css';
+import CreateEmployee from './pages/CreateEmployee';
+import EmployeeList from './pages/EmployeeList';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/create-employee" element={<CreateEmployee/>} />
+          <Route path="/employee-list" element={<EmployeeList/>} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux';
+
+// state
+const initialState = {
+
+};
+
+// actions creators
+
+
+
+// reducer
+
+function reducer(state = initialState, action) {
+
+}
+
+export const store = createStore(reducer);
